ca68224afb9d7049ae1b1615f292a08184967946
Zain Ul Mustafa <zain2984@gmail.com>
2017-08-16 18:12:32 +0500

    updated

--- Arduino2PC_SC/main.cpp
+++ Arduino2PC_SC/main.cpp
@@ -3,65 +3,49 @@
 //Rules:
 /*
 1. Check if connection with Arduino is made
-2. If Arduino is connected take input from user in a String
+2. If Arduino is connected then take input from user in a String
 3. Make a char array of the size of string + 1
 4. Copy the whole string into the char array
 5. Add escape sequence \n because this represents the end of a line in Serial Comm port
 6. Write the whole char array into the buffer (a buffer takes the info forward to the Serial Comm Port)
 7. Read from the Arduino if data is received successfully or if the action is taken as per needed
 */
+/* PC 2 Arduino */
 
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include<string>
-#include "SerialPort.h"
-
+#include<iostream>
 using namespace std;
+#include<string>
+#include<stdlib.h>
+#include"SerialPort.h"
 
-// String for getting the output from arduino
 char output[MAX_DATA_LENGTH];
-
-/* You must add these backslashes before writing your COM Portname */
-// I have COM3 on my PC
-char *port_name = "\\\\.\\COM3";
-
-// String for incoming data
 char incomingData[MAX_DATA_LENGTH];
 
+char *port = "\\\\.\\COM3";
+
 int main(){
-	SerialPort arduino(port_name);
-	if (arduino.isConnected()){
-		cout << "Connection Established" << endl;
+	SerialPort arduino(port);
+	if(arduino.isConnected()){
+		cout<<"Connection made"<<endl<<endl;
 	}
 	else{
-		cout << "ERROR, check port name";
+		cout<<"Error in port name"<<endl<<endl;
 	}
+	while(arduino.isConnected()){
+		cout<<"Enter your command: "<<endl;
+		string data;
+		cin>>data;
 
-	while (arduino.isConnected()){
-		cout << "Write something: \n";
-		string input_string;
-		// Getting input
-		getline(cin, input_string);
+		char *charArray = new char[data.size() + 1];
+		copy(data.begin(), data.end(), charArray);
+		charArray[data.size()] = '\n';
 
-		// Creating a charArray
-		char *charArray = new char[input_string.size() + 1];
-
-		// copying the input_string to charArray
-		copy(input_string.begin(), input_string.end(), charArray);
-
-		// Adding the delimiter -> \n states end of line in Serial Comm Port
-		charArray[input_string.size()] = '\n';
-
-		// Writing string to arduino
 		arduino.writeSerialPort(charArray, MAX_DATA_LENGTH);
-		// Getting reply/response from arduino
 		arduino.readSerialPort(output, MAX_DATA_LENGTH);
 
-		// printing the output
-		puts(output);
+		cout<<">> "<<output<<endl;
 
-		// freeing charArray memory
-		delete[] charArray;
+		delete [] charArray;
 	}
+	return 0;
 }
